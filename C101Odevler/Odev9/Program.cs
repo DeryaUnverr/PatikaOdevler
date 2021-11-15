7385e9b8da3ce2888fa16decf5a03878d4cc1e85
DeryaUnverr <unverderyaa@gmail.com>
2021-11-15 17:17:30 +0300

    Odev9- While-Foreach Döngü alıştırmaları yapıldı.

---
+++ C101Odevler/Odev9/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "While Döngüleri";
+            //bool durum = true;
+            //while (durum)
+            //{
+            //    Console.WriteLine("Hello Yazılım 131");
+            //    durum = false;
+            //}
+
+            //int i = 0;
+            //while (i<10)
+            //{
+            //    Console.WriteLine("Hello");
+            //    i++;
+            //}
+
+            //bool durum = true;
+            //while (durum)
+            //{
+            //    Console.WriteLine("1.Tamam 2.Devam");
+            //    char secim = Console.ReadKey(true).KeyChar;
+            //    if (secim == '1')
+            //        durum = false;
+            //}
+
+            //char secim2 = '2';
+            //while (secim2=='2')
+            //{
+            //    Console.WriteLine("1.Tamam 2.Devam");
+            //    secim2 = Console.ReadKey(true).KeyChar;
+            //}
+
+            //int i = 0;
+            //while (i<10)
+            //{
+            //    int j = 0;
+            //    while (j<10)
+            //    {
+            //        Console.Write("♥");
+            //        j++;
+            //    }
+            //    Console.WriteLine();
+            //    i++;
+            //}
+
+            // Foreach Kullanımı
+
+            //int[] sayiDizisi = { 2, 4, 5, 1, 2, 4 };
+            //int toplam = 0;
+
+            //foreach (int i in SayiDizisi)
+            //{
+            //    toplam += i;
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
