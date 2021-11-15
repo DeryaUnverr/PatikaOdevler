27128ebae783a22de72ed63b903e7e3bf018f3e1
DeryaUnverr <unverderyaa@gmail.com>
2021-11-15 17:32:55 +0300

    Odev11 - Array Sınıfı ve Methodlari kullanarak personel takip uygulaması geliştirdim.

---
+++ C101Odevler/Odev11/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] personeller = new string[0];
+
+            bool durum = true;
+            while (durum)
+            {
+                Console.WriteLine("1.Personel Ekle\n----\n2.Personelleri Görüntüle\n----\n3.Çıkış");
+                char secim = Console.ReadKey(true).KeyChar;
+
+                switch (secim)
+                {
+                    case '1':
+                        Array.Resize(ref personeller, personeller.Length + 1);
+                        Console.Write("Personel adı: ");
+                        string ad = Console.ReadLine();
+                        Console.Write("Personel soyadı: ");
+                        string soyad = Console.ReadLine();
+                        personeller[personeller.Length - 1] = ad + " " + soyad;
+                        break;
+                    case '2':
+                        Console.WriteLine("----\nPERSONEL LİSTESİ\n----");
+                        for (int i = 0; i < personeller.Length; i++)
+                        {
+                            Console.WriteLine((i + 1) + "." + personeller[i]);
+                        }
+                        Console.WriteLine("----");
+                        break;
+                    case '3':
+                        durum = false;
+                        break;
+                    default:
+                        Console.WriteLine("Lütfen geçerli bir seçim yapınız");
+                        break;
+                }
+            }
+        }
+    }
+}
