e52510babae50031a4614e36512a64c825dedd39
DeryaUnverr <unverderyaa@gmail.com>
2021-11-15 22:36:58 +0300

    Odev14 - Extension Metod ve recursif metod alıştırması yapıldı.

---
+++ C101Odevler/Odev14/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev14
+{
+    public static class DeryaExtension
+    {
+        public static IEnumerable<T> WhereDerya<T>(this IEnumerable<T> list, Func<T, bool> func)
+        {
+            List<T> YeniListe = new List<T>();
+            foreach (var item in list)
+            {
+                if (func(item))
+                {
+                    YeniListe.Add(item);
+                }
+            }
+            return YeniListe;
+        }
+    }
+
+    // Recursif Metod 
+    //public static int Faktoriyel(int sayi)
+    //{
+    //if (sayi <= 1)
+    //{
+    //    return 1;
+    //}
+    //return sayi * Faktoriyel(sayi - 1);
+    //}
+
+}
+
