2e682b4ef0a0a0daf5bdebd170e660492ab4a222
DeryaUnverr <unverderyaa@gmail.com>
2021-11-15 14:51:11 +0300

    C#101 - Odev3/Operatorler eksikler var

---
+++ C101Odevler/Odev3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Atama ve İşlemli Atama Operatörleri(=, +=, -=, *=, /=)
+             Mantıksal Operatörler(||, &&, !)
+             İlişkisel Operatörler(==,!=, <, >, >=,<=)
+             Aritmetik(+, -, *, /, %, ++, --)
+             */
+
+        }
+    }
+}
