0e2125e52e8ba91178128807588a2d93bd30ff70
DeryaUnverr <unverderyaa@gmail.com>
2021-11-15 14:37:28 +0300

    C# 101 - Değişkenler /Odev2 Yapıldı

--- C101Odevler/Odev2/Program.cs
+++ C101Odevler/Odev2/Program.cs
@@ -10,7 +10,36 @@ namespace Odev2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("odev2");
+            string sekil = " ---------------------------------";
+            string str1 = string.Empty;
+            str1 = "Derya Ünver ";
+            string ad = "Derya ";
+            string soyad = "Ünver";
+            string tamIsim = $"{ad} {soyad}";
+            Console.WriteLine(tamIsim);
+
+            Console.WriteLine(sekil);
+
+            int sayi1 = 5;
+            int sayi2 = 6;
+            int islem = sayi2 * sayi1;
+
+            bool kontrol = 10 > 2;
+          
+            //dönüşüm
+            string str20 = "20";
+            int int20 = 20;
+            string yeniDeger = str20 + int20.ToString();
+            //dateTime
+            string dateTime = DateTime.Now.ToString("dd.MM.yyyy");
+            Console.WriteLine(dateTime);
+            string dateTim2 = DateTime.Now.ToString("dd/MM/yyyy");
+            Console.WriteLine(dateTim2);
+            string hour = DateTime.Now.ToString("HH:mm");
+            Console.WriteLine(hour);
+
+
+            Console.ReadKey();
         }
     }
 }
