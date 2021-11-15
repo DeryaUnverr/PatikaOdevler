5b6457171077953901e7352070e595685758c07c
DeryaUnverr <unverderyaa@gmail.com>
2021-11-15 14:06:03 +0300

    Add project files.

---
+++ C101Odevler/Odev1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İsminizi Giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("SoyAdınızı Giriniz:");
+            string surname = Console.ReadLine();
+            Console.WriteLine($"Merhaba {name} {surname}");
+            Console.ReadKey();
+        }
+    }
+}
